61b498e3ab2adca99239d61c4ab3f6306c441dec
Brooke Robinson <brooke.robinson@colorado.edu>
2016-02-03 16:35:32 -0700

    added links

--- src/apps/parking/react/components/navbar.js
+++ src/apps/parking/react/components/navbar.js
@@ -3,11 +3,15 @@ MyComponents.NavBar = React.createClass({
     return (
       <nav>
         <div className="nav-wrapper">
-        <a href="#" className="brand-logo">Garage Viewer</a>
+        <a href="#" className="brand-logo">Parking Project</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="garages.html">Garages</a></li>
+        <li ><a href="team.html">Team Members</a></li>
+
+         // <li><a href="sass.html">Sass</a></li>
+         // <li><a href="badges.html">Components</a></li>
+         // <li><a href="collapsible.html">JavaScript</a></li>
         </ul>
         </div>
       </nav>
