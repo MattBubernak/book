9216147261b15e775c780be7c7edd94d15211dd5
Brooke Robinson <brooke.robinson@colorado.edu>
2016-02-03 16:35:56 -0700

    added image

--- src/apps/parking/react/components/navbar.js
+++ src/apps/parking/react/components/navbar.js
@@ -9,12 +9,24 @@ MyComponents.NavBar = React.createClass({
         <li><a href="garages.html">Garages</a></li>
         <li ><a href="team.html">Team Members</a></li>
 
+
          // <li><a href="sass.html">Sass</a></li>
          // <li><a href="badges.html">Components</a></li>
          // <li><a href="collapsible.html">JavaScript</a></li>
         </ul>
         </div>
       </nav>
+
+      <style>
+      html {
+        background: url(Free_Parking.jpg) no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+    </style>
+    
     );
   }
 });
