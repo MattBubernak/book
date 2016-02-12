39466e366f89ae78982a921526228c8c2d0cdf88
lilyJi <lili.ji@colorado.edu>
2016-02-12 16:51:21 -0700

    user login

--- src/apps/uber/client/components/navbar.jsx
+++ src/apps/uber/client/components/navbar.jsx
@@ -2,16 +2,17 @@ class NavBar extends React.Component {
 
   render(){
     return (
-     <nav className = "black">
-      <div className="nav-wrapper">
+    <nav>
+        <div className="nav-wrapper black darken-3">
         <a href="#" className="brand-logo center">Uber Cart</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="index.html">Home</a></li>
-          <li><a href="#">Login</a></li>
+        <li><a href="/apps/uber/index.html">Home</a></li>
+        <li><a href="/apps/uber/client">Clients</a></li>
+         <li><a href="/apps/uber/admin">Admins</a></li>
         </ul>
-		
+
       </div>
-     </nav>		  
+     </nav>
     );
   }
 
