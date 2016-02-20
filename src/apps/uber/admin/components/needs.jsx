7136bda3a236205890960acc5a7d7ffcb36d0a98
jcherrera <j.c.h@me.com>
2016-02-19 21:39:13 -0700

    beauty changes

--- src/apps/uber/admin/components/needs.jsx
+++ src/apps/uber/admin/components/needs.jsx
@@ -1,19 +1,19 @@
 class Needs extends React.Component {
     render() {
         return <div id="needs">
-            <h4>What do you want to see?</h4>
+            <h4>What are you looking for?</h4>
 			<i> Click on the items below to enable filters. </i>
             <form action="#">
                 <ul id="items">
                     <div className="row">
 						<div className="col s2"> 
 						<input onChange={this.props.actions.filterUpdateBurger} type="checkbox" id="s1" />
-                        <label htmlFor="s1">Burger</label>
+                        <label htmlFor="s1">Burgers</label>
 						</div>
 
 						<div className="col s2"> 
                         <input onChange={this.props.actions.filterUpdateHotDog} type="checkbox" id="s2" />
-                        <label htmlFor="s2">Hot Dog</label>
+                        <label htmlFor="s2">Hot Dogs</label>
 						</div>
 						<div className="col s2"> 
                         <input onChange={this.props.actions.filterUpdateIceCream} type="checkbox" id="s3" />
